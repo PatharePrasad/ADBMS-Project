57434450bbd290c670253d74ad6d975e760ca6e6
prasad373 <prasadsachinist10@gmail.com>
2022-09-23 21:08:30 +0530

    Initial Commit

---
+++ employee.py
@@ -0,0 +1,24 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+
+
+class Employee:
+    def __init__(self,root):
+          self.root=root
+          self.root.geometry("1530x790+0+0")
+          self.root.title('Employee Managament System')
+
+
+lbl_title=Label(self.root,text='EMPLOYEE MANAGEMENT SYSTEM',font=('times new roman',37,'bold'),fg='darkblue',bg='white')
+lbl_title.place(x=0,y=0,width=1530,height=50)
+
+
+
+
+
+if __name__=="__main__":
+    root=Tk()
+    obj=Employee(root)
+    root.mainloop()
+
