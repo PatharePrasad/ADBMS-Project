66235330599b764c81454684b266bc1bd1ce7540
prasad373 <prasadsachinist10@gmail.com>
2022-09-24 18:59:37 +0530

    Error solved

--- employee.py
+++ employee.py
@@ -1,21 +1,4 @@
 from tkinter import Label, Tk
-<<<<<<< HEAD
-
-
-class Employee(Tk):
-    def __init__(self):
-        super().__init__()
-
-        # Configuring the main window!
-        self.title('Employee Management System')
-        self.geometry("1530x790+0+0")
-
-        # Adding label 
-        self.label = Label(self, text="EMPLOYEE MANAGEMENT SYSTEM", font=('times new roman',37,'bold'),fg='darkblue',bg='white')
-        self.label.place(x=0,y=0,width=1530,height=50)
-
-=======
->>>>>>> 60928afc0d9f0984216339cdf125db2fcdc924e3
 
 
 class Employee(Tk):
@@ -32,11 +15,5 @@ class Employee(Tk):
 
 
 if __name__=="__main__":
-<<<<<<< HEAD
-        app = Employee()
-        app.mainloop()
-
-=======
     app = Employee()
     app.mainloop()
->>>>>>> 60928afc0d9f0984216339cdf125db2fcdc924e3
