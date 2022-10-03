9eae7d5406bd96b4ff48e9e96e6d82d32bb8c55f
prasad373 <prasadsachinist10@gmail.com>
2022-10-03 15:51:48 +0530

    Completed Employee Information Section

--- employee.py
+++ employee.py
@@ -1,5 +1,5 @@
 
-from tkinter import RIDGE, W, Frame, Image, Label, LabelFrame, Tk, font, ttk
+from tkinter import RIDGE, W, Button, Frame, Image, Label, LabelFrame, Tk, font, ttk
 from PIL import Image, ImageTk
 class Employee(Tk):
     def __init__(self):
@@ -106,9 +106,81 @@ class Employee(Tk):
         combo_txt_married.current(0)
         combo_txt_married.grid(row=2,column=3,padx=2,pady=7,sticky=W)
 
-        lbl_dep=Label(upper_frame,text='Department',font=('aerial',11,'bold'),bg='white')
-        lbl_dep.grid(row=0,column=0,padx=2,sticky=W)
-        combo_dep=ttk.Combobox(upper_frame,font=("aerial",12,"bold"),width=17,state='readonly')
+        # DOB
+        lbl_dob=Label(upper_frame,font=("aerial",12,"bold"),text="DOB:",bg="white")
+        lbl_dob.grid(row=3,column=0,sticky=W,padx=2,pady=7)
+
+        txt_dob=ttk.Entry(upper_frame,width=22,font=("aerial",11,"bold"))
+        txt_dob.grid(row=3,column=1,padx=2,pady=7)
+
+        # DOJ
+        lbl_doj=Label(upper_frame,font=("aerial",12,"bold"),text="DOJ:",bg="white")
+        lbl_doj.grid(row=3,column=2,sticky=W,padx=2,pady=7)
+
+        txt_doj=ttk.Entry(upper_frame,width=22,font=("aerial",11,"bold"))
+        txt_doj.grid(row=3,column=3,padx=2,pady=7)
+
+        # ID Proof
+        lbl_proof=Label(upper_frame,text='ID Proof',font=('aerial',12,'bold'),bg='white')
+        lbl_proof.grid(row=4,column=0,padx=2,sticky=W)
+
+        combo_txt_proof=ttk.Combobox(upper_frame,font=('aerial',12,'bold'),width=18,state='readonly')
+        combo_txt_proof['value']=("Select ID Proof", "PAN CARD", "AADHAR CARD", "DRIVING LICENCE")
+        combo_txt_proof.current(0)
+        combo_txt_proof.grid(row=4,column=1,padx=2,pady=7,sticky=W)
+
+        # Gender
+        lbl_gender=Label(upper_frame,text='Gender',font=('aerial',12,'bold'),bg='white')
+        lbl_gender.grid(row=4,column=2,padx=2,sticky=W)
+
+        combo_txt_gender=ttk.Combobox(upper_frame,font=('aerial',12,'bold'),width=18,state='readonly')
+        combo_txt_gender['value']=("Male", "Female", "Other")
+        combo_txt_gender.current(0)
+        combo_txt_gender.grid(row=4,column=3,padx=2,pady=7,sticky=W)
+
+         # Phone
+        lbl_phone=Label(upper_frame,font=("aerial",12,"bold"),text="Phone:",bg="white")
+        lbl_phone.grid(row=0,column=4,sticky=W,padx=2,pady=7)
+
+        txt_phone=ttk.Entry(upper_frame,width=22,font=("aerial",11,"bold"))
+        txt_phone.grid(row=0,column=5,padx=2,pady=7)
+
+        # Country
+        lbl_country=Label(upper_frame,font=("aerial",12,"bold"),text="Country:",bg="white")
+        lbl_country.grid(row=1,column=4,sticky=W,padx=2,pady=7)
+
+        txt_country=ttk.Entry(upper_frame,width=22,font=("aerial",11,"bold"))
+        txt_country.grid(row=1,column=5,padx=2,pady=7)
+
+        # Country
+        lbl_country=Label(upper_frame,font=("aerial",12,"bold"),text="Country:",bg="white")
+        lbl_country.grid(row=1,column=4,sticky=W,padx=2,pady=7)
+
+        txt_country=ttk.Entry(upper_frame,width=22,font=("aerial",11,"bold"))
+        txt_country.grid(row=1,column=5,padx=2,pady=7)
+
+        # CTC
+        lbl_ctc=Label(upper_frame,font=("aerial",12,"bold"),text="Salary(CTC):",bg="white")
+        lbl_ctc.grid(row=2,column=4,sticky=W,padx=2,pady=7)
+
+        txt_ctc=ttk.Entry(upper_frame,width=22,font=("aerial",11,"bold"))
+        txt_ctc.grid(row=2,column=5,padx=2,pady=7)
+
+        #Button Frame
+        button_frame=Frame(upper_frame,bd=2,relief=RIDGE,bg='white')
+        button_frame.place(x=1290,y=10,width=170,height=210)
+
+        btn_add=Button(button_frame,text="Save",font=("aerial",15,"bold"),width=13,bg='blue',fg='white')
+        btn_add.grid(row=0,column=0,padx=1,pady=5)
+ 
+        btn_update=Button(button_frame,text="Update",font=("aerial",15,"bold"),width=13,bg='blue',fg='white')
+        btn_update.grid(row=1,column=0,padx=1,pady=5)
+
+        btn_delete=Button(button_frame,text="Delete",font=("aerial",15,"bold"),width=13,bg='blue',fg='white')
+        btn_delete.grid(row=2,column=0,padx=1,pady=5)
+
+        btn_clear=Button(button_frame,text="Clear",font=("aerial",15,"bold"),width=13,bg='blue',fg='white')
+        btn_clear.grid(row=3,column=0,padx=1,pady=5)
 
         #down Frame
         down_frame=LabelFrame(Main_frame,bd=2,relief=RIDGE,bg='white', text='Employee Information Table',font=('times new roman',11,'bold'),fg='red')
